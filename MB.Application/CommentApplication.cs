7b245eca6d211760ab9ffcadf6a10e77d891828a
mjd166 <mghafari41@yahoo.com>
2022-04-21 08:32:22 +0330

    list comments implemented

--- MB.Application/CommentApplication.cs
+++ MB.Application/CommentApplication.cs
@@ -1,10 +1,6 @@
 ﻿using MB.Application.Contracts.Comment;
 using MB.Domain.CommentAgg;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace MB.Application
 {
@@ -21,6 +17,11 @@ namespace MB.Application
         {
             var comment = new Comment(command.Name, command.Email, command.Message, command.ArticleId);
             _commentRepository.CreateAndSave(comment);
+        }
+
+        public List<CommentViewModel> GetList()
+        {
+            return _commentRepository.GetList();
         }
     }
 }
--- MB.Infrastructure.EfCore/Repositories/CommentRepository.cs
+++ MB.Infrastructure.EfCore/Repositories/CommentRepository.cs
@@ -1,4 +1,10 @@
-﻿using MB.Domain.CommentAgg;
+﻿using MB.Application.Contracts.Comment;
+using MB.Domain.CommentAgg;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+
 namespace MB.Infrastructure.EfCore.Repositories
 {
     public class CommentRepository:ICommentRepository
@@ -14,6 +20,21 @@ namespace MB.Infrastructure.EfCore.Repositories
         {
             _context.Comments.Add(comment);
             _context.SaveChanges();
+        }
+
+        public List<CommentViewModel> GetList()
+        {
+            return _context.Comments.Include(x => x.Article).Select(x => new CommentViewModel
+            {
+                Id = x.Id,
+                Name = x.Name,
+                Email = x.Email,
+                Message = x.Message,
+                Status = x.Status,
+                CreationDate = x.CreationDate.ToString(CultureInfo.InvariantCulture),
+                Article = x.Article.Title
+
+            }).ToList();
         }
     }
 }
